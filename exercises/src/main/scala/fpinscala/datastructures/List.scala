597bc9448d09ef8d807327eba2edf8b1f21ac64d
RAZAFIMAHEFA Aina <nasoloaina@gmail.com>
2018-09-05 13:11:10 +0200

    Provide answer to exercise 3.10 : Implement foldLeft

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -248,7 +248,11 @@ object List { // `List` companion object. Contains functions for creating and wo
    */
   def length[A](l: List[A]): Int = foldRight(l, 0)((_, b) => 1 + b)
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = l match {
+    case Nil => z
+    case Cons(head, tail) => foldLeft(tail, f(z, head))(f)
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
